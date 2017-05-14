68f0bbf1ae9241ae889f7e20c48488a279877005
Silvia Teona M <silviateona@gmail.com>
2017-05-14 02:50:03 -0400

    cleaned up some files and comments to get ready to advance with notes app

--- app.js
+++ app.js
@@ -2,37 +2,14 @@ console.log('starting app.js, yo!');
 
 //fetch contents of fs aka file system nodejs module and put in constant
 const zeFS = require('fs');
-//fetch the Operating System module
-const zeOS = require('os');
 
 //require our very own notes.js file, with a relative path to the current file
 //requiring this file will run all code written in it!
 const notes = require('./notes.js');
 
-//userInfo() is a method from the OS module
-//it returns an object with properties such as the user name and home directory
-var user = zeOS.userInfo();
-//console.log(user); //prints an object
-console.log(`Hey there ${user.username}! Your old ass is already ${notes.age}!`);
-
 //making use of the addNote function in the notes.js file:
 var notesAddNote = notes.addNote();
 
-var addedNumbers = notes.addNumbers(1, 2);
-zeFS.appendFile('greetings.txt', `Ze added numbers total ${addedNumbers}.`,(err)=>{
-   if (err){
-      console.log('error adding numbers');
-   };
-});
-
 //let's include a third party library installed through npm into project!!
-//don't forget to intitialize github and its repository before doing an npm init in a project folder
-//here we used npm install lodash --save, the --save flag updating the package.json file
-//use package name as specified in this package.json file
-//third party modules install in a new node_modules folder
 
 const _=require('lodash');
-//let's use a method from the lodash api, uniq, that filters out all duplicates from an array
-var arrayFilter = ['silf', 'shroom', 'shrooms', 1, 2, 1, 2, 3];
-arrayFilter = _.uniq(arrayFilter);
-console.log(`filtered array: ${arrayFilter}`);
--- notes.js
+++ notes.js
@@ -1,19 +1,7 @@
 console.log('starting notes.js, yo!');
 //all files have access to the module object; console.log it to view properties
 
-//console.log(module);
-
-//the module object has an exports object property, which we can access from other files
-//by using the variable in which we required this file, ex. notes.age
-module.exports.age = 31;
-
-//and now, let's create an anonymous function for exports
-//anonymous functions are by default considered for errors
 module.exports.addNote = ()=>{
    console.log('ze addNote function accessed');
    return 'A brand-new note returned here, yo!';
 };
-
-module.exports.addNumbers = (a, b)=>{
-   return a+b;
-}
