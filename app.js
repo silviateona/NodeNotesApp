4da48d1c6a43026e9385008e0c9dbe5a3d743c6f
Silvia Teona M <silviateona@gmail.com>
2017-05-14 00:37:19 -0400

    why atom git not working...

--- app.js
+++ app.js
@@ -17,18 +17,3 @@ console.log(`Hey there ${user.username}! Your old ass is already ${notes.age}!`)
 
 //making use of the addNote function in the notes.js file:
 var notesAddNote = notes.addNote();
-
-/*
-appendFile is a method from the File System module (fs)
-the \r is a return to get a new line
-ze traditional way to write this:
-'\rHello there, '+user.username+'! This be JS fooling with yer files.'
-*/
-
-/*
-zeFS.appendFile('greetings.txt', `\rHey there, ${user.username}! Ain't ES6 nice???`, (err)=>{
-   if(err){
-      console.log('ze error meanz no writings to greetings file!');
-   }
-});
-*/
